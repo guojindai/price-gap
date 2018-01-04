d99eaf9cb298b9a40026e1c964fe84c83aec7782
bigpan <guojindai@163.com>
2018-01-04 22:39:02 +0800

    获取价格数据

--- data.js
+++ data.js
@@ -1,20 +1,86 @@
 const fs = require('fs');
 const path = require('path');
 const request = require('request-promise');
+const cheerio = require('cheerio');
+const moment = require('moment');
+
+const RE_PRICE = /([0-9,]+)/;
+const RE_ALL_COMMA = /,/g;
 
 const dataDir = path.resolve(__dirname, './data');
 if (!fs.existsSync(dataDir)){
   fs.mkdirSync(dataDir);
 }
 
+function throwNotNumberError(val) {
+  throw new Error(`throwNotNumberError: ${val} is not a integer number`);
+}
+
+function formatPrice(val) {
+  const found = val.match(RE_PRICE);
+  if (found) {
+    const intVal = parseInt(found[0].replace(RE_ALL_COMMA, ''));
+    if (intVal > 0) {
+      return intVal;
+    } else {
+      throwNotNumberError(val);
+    }
+  } else {
+    throwNotNumberError(val);
+  }
+}
+
+function getNowTimeString() {
+  return moment().format('YYYY-MM-DD HH:mm:ss');
+}
+
+async function getInPrices() {
+  const inPrices = {};
+  const resBody = await request('https://otcbtc.com/');
+  const $ = cheerio.load(resBody);
+  const $trs = $('.lp-section-2-coin').eq(0).find('tr');
+  inPrices.BTC = formatPrice($trs.eq(1).find('td').eq(2).text());
+  inPrices.ETH = formatPrice($trs.eq(2).find('td').eq(2).text());
+  inPrices.EOS = formatPrice($trs.eq(3).find('td').eq(2).text());
+  return inPrices;
+}
+
+async function getBTCOutPrice() {
+  const resBody = await request('https://otcbtc.com/sell_offers?currency=btc&fiat_currency=cny&payment_type=all&sort_by=most_trust');
+  const $ = cheerio.load(resBody);
+  return formatPrice($('.long-solution-list .list-content').eq(0).find('.price').text());
+}
+
+async function getETHOutPrice() {
+  const resBody = await request('https://otcbtc.com/sell_offers?currency=eth&fiat_currency=cny&payment_type=all&sort_by=most_trust');
+  const $ = cheerio.load(resBody);
+  return formatPrice($('.long-solution-list .list-content').eq(0).find('.price').text());
+}
+
+async function getEOSOutPrice() {
+  const resBody = await request('https://otcbtc.com/sell_offers?currency=eos&fiat_currency=cny&payment_type=all&sort_by=most_trust');
+  const $ = cheerio.load(resBody);
+  return formatPrice($('.long-solution-list .list-content').eq(0).find('.price').text());
+}
+
 async function getData() {
-  const resBody = await request('http://www.baidu.com');
-  fs.appendFileSync(path.resolve(dataDir, 'd1.csv'), resBody);
-  return resBody;
+  const inPrices = await getInPrices();
+  const btcOutPrice = await getBTCOutPrice();
+  const ethOutPrice = await getETHOutPrice();
+  const eosOutPrice = await getEOSOutPrice();
+  return {
+    inPrices,
+    btcOutPrice,
+    ethOutPrice,
+    eosOutPrice,
+  };
 }
 
 getData().then((data) => {
-  console.log(data);
+  const now = getNowTimeString();
+  fs.appendFileSync(path.resolve(dataDir, 'BTC.csv'), `${now},${data.inPrices.BTC},${data.btcOutPrice}\n`);
+  fs.appendFileSync(path.resolve(dataDir, 'ETH.csv'), `${now},${data.inPrices.ETH},${data.ethOutPrice}\n`);
+  fs.appendFileSync(path.resolve(dataDir, 'EOS.csv'), `${now},${data.inPrices.EOS},${data.eosOutPrice}\n`);
 }).catch((err) => {
-  console.error(err);
+  fs.appendFileSync(path.resolve(dataDir, 'data.log'), `${getNowTimeString()}, ${JSON.stringify(err)}`);
 });
