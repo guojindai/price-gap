27b519bd6a6da391051aae2674f68147d0b02a10
bigpan <guojindai@163.com>
2018-03-12 20:49:32 +0800

    指定中文页面

--- data.js
+++ data.js
@@ -41,15 +41,20 @@ async function getPrices() {
     inPrices: {},
     outPrices: {},
   };
-  const resBody = await request('https://otcbtc.com/');
+  const resBody = await request('https://otcbtc.com/', {
+    headers: {
+      'Cookie': 'locale=zh-CN;',
+    },
+  });
+  console.log(resBody);
   const $ = cheerio.load(resBody);
   const $trs = $('.lp-section-2-coin').eq(0).find('.lp-coin-list-content');
-  prices.outPrices.BTC = formatPrice($trs.eq(0).find('.lp-coin-list-fiat-highest-price').text());
-  prices.inPrices.BTC = formatPrice($trs.eq(0).find('.lp-coin-list-trading-latest-price .lp-coin-list-trading-fiat-price').text());
+  prices.outPrices.BTC = formatPrice($trs.eq(2).find('.lp-coin-list-fiat-highest-price').text());
+  prices.inPrices.BTC = formatPrice($trs.eq(2).find('.lp-coin-list-trading-latest-price .lp-coin-list-trading-fiat-price').text());
   // prices.outPrices.ETH = formatPrice($trs.eq(1).find('.lp-coin-list-fiat-highest-price').text());
   // prices.inPrices.ETH = formatPrice($trs.eq(1).find('.lp-coin-list-trading-latest-price .lp-coin-list-trading-fiat-price').text());
-  prices.outPrices.EOS = formatPrice($trs.eq(2).find('.lp-coin-list-fiat-highest-price').text());
-  prices.inPrices.EOS = formatPrice($trs.eq(2).find('.lp-coin-list-trading-latest-price .lp-coin-list-trading-fiat-price').text());
+  prices.outPrices.EOS = formatPrice($trs.eq(4).find('.lp-coin-list-fiat-highest-price').text());
+  prices.inPrices.EOS = formatPrice($trs.eq(4).find('.lp-coin-list-trading-latest-price .lp-coin-list-trading-fiat-price').text());
   return prices;
 }
 
